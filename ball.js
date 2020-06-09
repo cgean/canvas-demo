e23c5971152124464ad9bf8b123c013f0dcb7a37
Carlos <cgean.sp@gmail.com>
2020-06-08 21:22:00 -0300

    add mov and refactoring

--- ball.js
+++ ball.js
@@ -1,22 +1,49 @@
 class Ball {
 
     constructor(ctx) {
-        this.ctx = ctx;
-        this.width = ctx.canvas.clientWidth / 10;
-        this.height = this.width;
-        this.x = 50;
-        this.y = 50;
-        console.log("Ball created");
+        this.step = 10
+        this.ctx = ctx
+        this.canvasWidth = ctx.canvas.clientWidth
+        this.canvasHeight = ctx.canvas.clientHeight
+        this.width = this.canvasWidth / this.step
+        this.height = this.width
+        this.radius = this.width / 2
+        this.x = 50
+        this.y = 50
+        console.log("Ball created")
     }
 
     draw() {
-        this.ctx.beginPath();
-        this.ctx.arc(this.x, this.y, this.width / 2, 0, 2 * Math.PI, false);
-        this.ctx.lineWidth = 3;
-        this.ctx.fillStyle = "#00FF00";
-        this.ctx.fill();
-        this.ctx.strokeStyle = '#FF0000';
-        this.ctx.stroke();
-        console.log("Ball drawned");
+        this.ctx.beginPath()
+        this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false)
+        this.ctx.lineWidth = 3
+        this.ctx.fillStyle = "rgb(80,80,80)"
+        this.ctx.fill()
+        this.ctx.strokeStyle = "rgb(150,150,150)"
+        this.ctx.stroke()
+    }
+
+    up(){
+        if (this.y - this.radius > 0){
+            this.y -= this.step
+        }
+    }
+
+    down(){
+        if (this.y + this.radius < this.canvasHeight){
+            this.y += this.step
+        }       
+    }
+    
+    left(){
+        if (this.x - this.radius > 0){
+            this.x -= this.step
+        }
+    }
+
+    right(){
+        if (this.x + this.radius < this.canvasWidth){
+            this.x += this.step
+        }
     }
 }
--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-console.log("Started");
+console.log("Started")
 
-let ball;
+let canvas
+let ball
 
 function start() {
-    let canvas = document.getElementById('mycanvas');
-    let ctx = canvas.getContext("2d");
-    let ball = new Ball(ctx);
-    ball.draw();
+    canvas = document.getElementById('mycanvas')
+    let ctx = canvas.getContext("2d")
+    ball = new Ball(ctx)
+    draw()
 }
 
+function draw() { 
+    let ctx = canvas.getContext("2d") 
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ball.draw()
+}
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowUp') {
+        ball.up()
+    } else if (event.key === 'ArrowDown') {
+        ball.down()      
+    } else if (event.key === 'ArrowLeft') {
+        ball.left()
+    } else if (event.key === 'ArrowRight') {
+        ball.right()     
+    }
+    draw()
+ });
